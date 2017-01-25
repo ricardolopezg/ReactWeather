cf981a129d4abed6f0641364a51fd59b4e40e884
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2017-01-24 23:15:22 -0500

    fixed extra = in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 // If http, passess. If https, changes to http.
 app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] ==== 'http') {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
