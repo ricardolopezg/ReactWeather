349244e504602326872526c4cf44d95a555757a2
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2017-01-26 12:59:06 -0500

    about style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,12 +8,27 @@ var React = require('react');
 //   }
 // });
 
+var border = {
+  border: '2px red'
+}
+
 // Refactored
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About This App</h1>
+      <p>
+        I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React. I built this in React.
+      </p>
+      <p>I used these tools:</p>
+      <ol>
+        <li>
+          <a href = "https://facebook.github.io/react/">React</a> - JS Framework used.
+        </li>
+        <li>
+          <a href = "http://openweathermap.org">Open Weather Map</a> - Used to call Weather API.
+        </li>
+      </ol>
     </div>
   );
 }
