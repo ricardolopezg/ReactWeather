87c7269e55ca5f80077be42dc9dd6d3d4e911952
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2017-01-25 21:01:05 -0500

    fixed local host issue

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 // If http, passess. If https, changes to http.
 app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
